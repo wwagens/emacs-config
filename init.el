4b213fdeb769432fad922339df1bd2f8e6401d92
wwagens <103839163+wwagens@users.noreply.github.com>
2022-04-26 18:15:31 +0200

    Update init.el

--- init.el
+++ init.el
@@ -8,14 +8,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(defun fontify-frame (frame)
-  (set-frame-parameter frame 'font "Consolas-11"))
-;; Fontify current frame
-(fontify-frame nil)
-;; Fontify any future frames
-(push 'fontify-frame after-make-frame-functions)
-
-;; (load-theme 'wombat)
 
 
 (set-frame-parameter (selected-frame) 'alpha '(95 95)) ;;Activates transpareci
@@ -37,7 +29,7 @@
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-   (package-install 'use-package))
+  (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -56,4 +48,96 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil use-package)))
+ '(custom-safe-themes
+   (quote
+    ("cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" default)))
+ '(package-selected-packages
+   (quote
+    (doom-themes rainbow-delimiters doom-modeline ggtags yasnippet flycheck auto-complete which-key cmake-ide evil use-package))))
+
+(use-package which-key
+:ensure t
+:config
+(which-key-mode))
+
+
+;; Auto completion
+(use-package auto-complete
+:ensure t
+:init
+(progn
+(ac-config-default)
+(global-auto-complete-mode t)
+))
+
+;; A nice looking modeline enhancement
+(use-package spaceline
+  :ensure t)
+
+;; Add some visual flair to the modeline enhancements
+(use-package spaceline-all-the-icons
+  :ensure t
+  :after spaceline
+ :config (spaceline-all-the-icons-theme)
+  (spaceline-all-the-icons--setup-neotree))
+
+
+;; on the fly syntax checking
+(use-package flycheck
+:ensure t
+:init
+(global-flycheck-mode t))
+
+;; snippets and snippet expansion
+(use-package yasnippet
+:ensure t
+:init
+(yas-global-mode 1))
+
+
+;; tags for code navigation
+(use-package ggtags
+:ensure t
+:config
+(add-hook 'c-mode-common-hook
+(lambda ()
+(when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+  (ggtags-mode 1))))
+)
+
+(column-number-mode)
+(global-display-line-numbers-mode)
+
+(dolist (mode '(org-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook))
+(add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package rainbow-delimiters :hook(prog-mode . rainbow-delimiters-mode))
+
+(load-theme 'doom-nord)
+
+(electric-pair-mode 1)
+(setq electric-pair-preserve-balance nil)
+
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+    ;; golang
+    (el-get-bundle go-mode)(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+    ;; golang
+    (el-get-bundle go-mode)
